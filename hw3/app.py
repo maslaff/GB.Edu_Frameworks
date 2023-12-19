93109af007eeffe94a5b85b2c9ae58f4917dd44d
MaSlaff <maslaff@yandex.ru>
2023-12-19 23:58:22 +0300

    hw3 solved

--- hw3/app.py
+++ hw3/app.py
@@ -8,16 +8,19 @@ from flask import (
     url_for,
 )
 
-from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from werkzeug.security import generate_password_hash, check_password_hash
-from model import db, User
-from forms import LoginForm
+from hw3.model import db, User
+from hw3.forms import LoginForm, RegistrationForm
+from sqlalchemy.exc import IntegrityError
+
 
 app = Flask(__name__)
 
 app.secret_key = "5f214cacbd30c2a0f17912ae0d5d8c16ae9812846221265e4"
-
+app.config["SECRET_KEY"] = "a0f17912ae0d5d8c16ae981"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+csrf = CSRFProtect(app)
 db.init_app(app)
 
 
@@ -38,35 +41,32 @@ def index():
 
 @app.route("/login/", methods=["POST", "GET"])
 def login():
-    if request.method == "POST":
-        # users = User.query.all()
-        # print(users)
-
-        # session["username"] = request.form.get("username") or "NoName"
-        form_email = request.form.get("email")
-        form_password = request.form.get("password")
+    form = LoginForm()
+    if request.method == "POST" and form.validate():
+        form_email = form.email.data
+        form_password = form.password.data
         usr_d = User.query.filter(User.email == form_email).first()
         if usr_d and check_password_hash(usr_d.password, form_password):
             flash(f"User {form_email} logged in", "success")
             response = make_response(redirect(url_for("index")))
-            # response.set_cookie("username", form_uname)
             response.set_cookie("email", form_email)
             return response
 
         flash("Bad email or password", "warning")
         return redirect(url_for("login"))
     else:
-        return render_template("login.html")
+        return render_template("login.html", form=form)
 
 
 @app.route("/registration/", methods=["POST", "GET"])
 def registration():
-    if request.method == "POST":
+    form = RegistrationForm()
+    if request.method == "POST" and form.validate():
         # session["username"] = request.form.get("username") or "NoName"
-        form_fname = request.form.get("first_name")
-        form_lname = request.form.get("last_name")
-        form_email = request.form.get("email")
-        form_password = request.form.get("password")
+        form_fname = form.fname.data
+        form_lname = form.lname.data
+        form_email = form.email.data
+        form_password = form.password.data
 
         usr = User(
             first_name=form_fname,
@@ -74,8 +74,14 @@ def registration():
             email=form_email,
             password=generate_password_hash(form_password),
         )
-        db.session.add(usr)
-        db.session.commit()
+        try:
+            db.session.add(usr)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            flash(f"User {form_email} already registered", "warning")
+            return redirect(url_for("registration"))
+
         print(f"User {form_fname} {form_lname} registered")
 
         flash(f"User {form_email} registered", "success")
@@ -85,12 +91,12 @@ def registration():
         return response
         # return redirect(url_for("index"))
     else:
-        return render_template("registration.html")
+        return render_template("registration.html", form=form)
 
 
 @app.route("/logout/")
 def logout():
-    name = request.cookies.get("username")
+    name = request.cookies.get("email")
     response = make_response(redirect(url_for("index")))
     response.delete_cookie("username")
     response.delete_cookie("email")
--- hw3/forms.py
+++ hw3/forms.py
@@ -1,11 +1,46 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
+
+
+class Include(object):
+    def __init__(self, digits=True, capital=True, message=None):
+        self.digits = digits
+        self.capital = capital
+        if not message:
+            message = f"Field must be include {'digits' if digits else ''} {'capital letters' if capital else ''}."
+        self.message = message
+
+    def __call__(self, form, field):
+        d = field.data
+        print(f"VALIDATORS DATA!!! --- {d}")
+        if (self.digits and any(map(str.isdigit, d))) and (
+            self.capital and any(map(str.isupper, d))
+        ):
+            print("VALIDATED!!!")
+            return
+        print("NOT VALIDATED!!!")
+        raise ValidationError(self.message)
 
 
 class LoginForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
-    confirm_password = PasswordField(
-        "Confirm password", validators=[DataRequired(), EqualTo("password")]
+    password = PasswordField("Password", validators=[DataRequired()])
+
+
+class RegistrationForm(FlaskForm):
+    fname = StringField("First name", validators=[DataRequired()])
+    lname = StringField("Last name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=6),
+            EqualTo(
+                "confirm_password", message="Вайвай"
+            ),  # ??? Сообщение не выводится!
+            Include(),
+        ],
     )
+    confirm_password = PasswordField("Confirm password")
