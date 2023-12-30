da20aed4c21cd10f4ea8d0389a7db58750a065f8
MaSlaff <maslaff@yandex.ru>
2023-12-31 01:18:28 +0300

    hw4 mod optimize

--- hw4/app.py
+++ hw4/app.py
@@ -28,44 +28,11 @@ def timeof(func):
     return wrapper
 
 
-class func:
-    def __init__(self):
-        self.final_numlist = []
-
-    def sum_func(self, numlist):
-        # self.final_numlist.append(sum(numlist))
-        self.final_numlist.append(synch_sum(numlist))
-
-
 def synch_sum(numlist):
     acc = 0
     for i in numlist:
         acc += i
-    # print(f"Final sum is {acc}")
     return acc
-
-
-@timeof
-def thread_sum(numlist, part_size=100):
-    import threading
-
-    print(f"\n\nThread------")
-
-    while len(numlist) > 1:
-        threads = []
-        main_func = func()
-        for i in range(0, len(numlist), part_size):
-            thread = threading.Thread(
-                target=main_func.sum_func,
-                args=[numlist[i : i + part_size]],
-            )
-            threads.append(thread)
-            thread.start()
-        for thread in threads:
-            thread.join()
-        numlist = main_func.final_numlist
-
-    print(f"Final sum is {main_func.final_numlist[0]}")
 
 
 def m_func(numlist, acc_list):
@@ -73,12 +40,10 @@ def m_func(numlist, acc_list):
 
 
 @timeof
-def thread_sum2(numlist, parts=5):
+def thread_sum(numlist, parts=5):
     import threading
 
     acc_list = []
-
-    print(f"\n\nThread------")
 
     while len(numlist) > parts:
         threads = []
@@ -95,7 +60,7 @@ def thread_sum2(numlist, parts=5):
             thread.join()
         numlist = acc_list
 
-    print(f"Final sum is {synch_sum(acc_list)}")
+    return synch_sum(acc_list)
 
 
 @timeof
@@ -104,8 +69,6 @@ def multiproc_sum(numlist, parts=5):
 
     manager = Manager()
     acc_list = manager.list()
-
-    print(f"\n\nMultiproc------")
 
     while len(numlist) > parts:
         processes = []
@@ -122,14 +85,12 @@ def multiproc_sum(numlist, parts=5):
             process.join()
         numlist = acc_list
 
-    print(f"Final sum is {synch_sum(acc_list)}")
+    return synch_sum(acc_list)
 
 
 @timeof
 def async_sum(numlist, parts=5):
     import asyncio
-
-    print(f"\n\nAsync------")
 
     acc_list = []
 
@@ -150,36 +111,53 @@ def async_sum(numlist, parts=5):
         await asyncio.gather(*tasks)
 
     asyncio.run(main())
-    # print(asyncio.run(synch_sum(acc_list)))
 
-    print(f"Final sum is {asyncio.run(synch_sum(acc_list))}")
+    return asyncio.run(synch_sum(acc_list))
 
 
-@timeof
-def printsum(lst):
-    print(f"Final sum is {sum(lst)}")
+def main():
+    fin_str = "Final sum is {}"
+    tme_str = "Longer than {} times:\n{:>25.02f}"
+
+    randnum_list = [random.randint(min_value, max_value) for _ in range(numcount)]
+
+    sum_func = timeof(sum)
+    tme_sum_result, tme_sum = sum_func(randnum_list)
+    print(fin_str.format(tme_sum_result))
+
+    print("\n\nSync------")
+    sync_sum_func = timeof(synch_sum)
+    tme_sync_sum_result, tme_sync_sum = sync_sum_func(randnum_list)
+    print(fin_str.format(tme_sync_sum_result))
+    print(tme_str.format("'str'", tme_sync_sum / tme_sum))
+
+    print("\n\nThread------")
+    tme_thread_sum_result, tme_thread_sum = thread_sum(randnum_list)
+    print(fin_str.format(tme_thread_sum_result))
+    print(tme_str.format("Sync", tme_thread_sum / tme_sync_sum))
+
+    print("\n\nMultiproc------")
+    tme_multiproc_sum_result, tme_multiproc_sum = multiproc_sum(randnum_list)
+    print(fin_str.format(tme_multiproc_sum_result))
+    print(tme_str.format("Sync", tme_multiproc_sum / tme_sync_sum))
+
+    print("\n\nAsync------")
+    tme_async_sum_result, tme_async_sum = async_sum(randnum_list)
+    print(fin_str.format(tme_async_sum_result))
+    print(tme_str.format("Sync", tme_async_sum / tme_sync_sum))
+
+    print("\n")
+    if (
+        tme_sum_result
+        == tme_sync_sum_result
+        == tme_thread_sum_result
+        == tme_multiproc_sum_result
+        == tme_async_sum_result
+    ):
+        print("sums is OK")
+    else:
+        print("one or more sums is bad")
 
 
 if __name__ == "__main__":
-    rand_list = [random.randint(min_value, max_value) for _ in range(numcount)]
-    # rand_list = [i for i in range(min_value, max_value + 1)]
-    rl = rand_list
-    tme_sum = printsum(rl)[1]
-
-    print(f"\n\nSync------")
-    sync_sum_func = timeof(synch_sum)
-    rl = rand_list
-    tme_sync_sum_result, tme_sync_sum = sync_sum_func(rl)
-    print(f"Final sum is {tme_sync_sum_result}")
-    print(f"Longer than 'sum' times:\n{tme_sync_sum / tme_sum:>25.02f}")
-    del sync_sum_func
-
-    rl = rand_list
-    tme_thread_sum = thread_sum2(rl)[1]
-    print(f"Longer than Synch times:\n{tme_thread_sum / tme_sync_sum:>25.02f}")
-
-    rl = rand_list
-    tme_multiproc_sum = multiproc_sum(rl)[1]
-    print(f"Longer than Synch times:\n{tme_multiproc_sum / tme_sync_sum:>25.02f}")
-    tme_async_sum = async_sum(rl)[1]
-    print(f"Longer than Synch times:\n{tme_async_sum / tme_sync_sum:>25.02f}")
+    main()
